8d0b88e11d6f283f4e17689a01af5e68c6591e77
Бордачёв Виталий <vitalek-sky@yandex.ru>
2022-05-26 09:19:25 +0800

    26052022

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,35 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<bitmap/BitmapPlugin.h>)
+#import <bitmap/BitmapPlugin.h>
+#else
+@import bitmap;
+#endif
+
+#if __has_include(<url_launcher_ios/FLTURLLauncherPlugin.h>)
+#import <url_launcher_ios/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher_ios;
+#endif
+
+#if __has_include(<yandex_mapkit/YandexMapkitPlugin.h>)
+#import <yandex_mapkit/YandexMapkitPlugin.h>
+#else
+@import yandex_mapkit;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BitmapPlugin registerWithRegistrar:[registry registrarForPlugin:@"BitmapPlugin"]];
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+  [YandexMapkitPlugin registerWithRegistrar:[registry registrarForPlugin:@"YandexMapkitPlugin"]];
+}
+
+@end
