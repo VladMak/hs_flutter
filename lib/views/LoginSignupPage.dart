d54dc73db5e071d6da8046068d96ba03faa86dad
Бордачёв Виталий <vitalek-sky@yandex.ru>
2022-07-15 00:27:37 +0800

    проверка почты на

--- lib/views/LoginSignupPage.dart
+++ lib/views/LoginSignupPage.dart
@@ -53,6 +53,8 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
   Color focusColor = Color.fromARGB(0xFF, 0xB3, 0x19, 0x18);
   late FocusNode _focusNode;
 
+  TextEditingController _textFieldController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -167,7 +169,7 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
       child: _formMode == FormMode.LOGIN
           ? new Text('Создать аккаунт',
               style: new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500))
-          : new Text('Уже зарегестрированы? Войти',
+          : new Text('Уже зарегистрированы? Войти',
               style:
                   new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500)),
       onPressed: _formMode == FormMode.LOGIN ? showSignupForm : showLoginForm,
@@ -195,10 +197,10 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
       return new Text(
         _errorMessage,
         style: TextStyle(
-            fontSize: 13.0,
+            fontSize: 18.0,
             color: Colors.red,
             height: 1.0,
-            fontWeight: FontWeight.w300),
+            fontWeight: FontWeight.w700),
       );
     } else {
       return new Container(
@@ -260,7 +262,26 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
           var logged = await api.registration(
               name: _name, email: _email, pswd: _password);
           print("LOGGED {$logged}");
-          if (logged != "false") {
+          await _confirmCodeDialog(context);
+          if (_textFieldController.text == logged) {
+            // Коды совпали
+            Navigator.of(widget.queue.removeLast()).pushReplacement(
+                MaterialPageRoute(builder: (context) => Cabinet()));
+            widget.queue.addLast(
+                keyFragmentBody.currentState?.getContext() as BuildContext);
+            widget.updateTitle(screenTitles[Screen.Cabinet]);
+
+            setState(() {
+              _isLoading = false;
+            });
+          }
+          else{
+            setState(() {
+              _isLoading = false;
+              _errorMessage = "Неверный код!";
+            });
+          }
+          /*if (logged != "false") {
             // СЮДА в случае успешного входа, перекинуть на главную страницу, или на Кабинет
             Navigator.of(widget.queue.removeLast()).pushReplacement(
                 MaterialPageRoute(builder: (context) => Cabinet()));
@@ -285,11 +306,8 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
                 ],
               ),
             );
-          }
+          }*/
         }
-        setState(() {
-          _isLoading = false;
-        });
 
         if (userId.length > 0 && userId != null) {
           //widget.onSignedIn();
@@ -309,5 +327,35 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
         _isLoading = false;
       });
     }
+  }
+
+  Future<void> _confirmCodeDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Подтверждение электронной почты'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: InputDecoration(hintText: "Введите код подтверждения"),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Отмена'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text('Подтвердить'),
+              onPressed: () {
+                print(_textFieldController.text);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
